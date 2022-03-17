48d8beb9982d745a3113b9ea18768393e536a468
aurelticot <aurelticot.dev@gmail.com>
2022-03-17 18:52:30 +1100

    use Verida datastore to manage settings

--- src/lib/contexts/UserSettingsContext.tsx
+++ src/lib/contexts/UserSettingsContext.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import { config } from "config";
-import { UserSettings } from "lib/types";
+import { UserSettings, SettingsRecord } from "lib/types";
 import { recordTelemetryBreadcrumb, getLogger } from "lib/utils";
+import { useVerida } from "lib/hooks";
 const logger = getLogger("UserSettings");
 
 const { defaultUserSettings, settings } = config;
@@ -43,19 +44,133 @@ export const UserSettingsContext = React.createContext({
 });
 
 export const UserSettingsProvider: React.FunctionComponent = (props) => {
+  const datastoreInitialised = useRef(false);
+  const { datastore } = useVerida();
   const [userSettings, setUserSettings] =
     useState<UserSettings>(initialUserSettings);
 
   useEffect(() => {
-    logger.info(`User Settings set`, userSettings);
-    if (!localStorage) {
+    if (!datastore) {
+      datastoreInitialised.current = false;
       return;
     }
-    localStorage.setItem(
-      userSettingsLocalStorageKey,
-      JSON.stringify(userSettings)
-    );
-  }, [userSettings]);
+    const initialisationHandler = async () => {
+      logger.debug(`User Settings datastore initialisation handler`);
+      // First time using the datastore, if empty, initialise it with the
+      // current settings, otherwise override the current settings with the
+      // settings from the datastore
+
+      const settingsRecords = await datastore.getMany();
+      if (!settingsRecords || settingsRecords.length === 0) {
+        logger.debug(
+          `No Settings record found, initialising with current settings in browser`
+        );
+        // TODO handle errors
+        // TODO find a better way to cast the property types
+        const recordToSave: Omit<SettingsRecord, "_id" | "_rev"> = {
+          language: userSettings.language as string,
+          theme: userSettings.theme as
+            | "system"
+            | "black"
+            | "dark"
+            | "light"
+            | "white",
+          marketSelection: userSettings.marketSelection as string[],
+          marketSort: userSettings.marketSort as
+            | "ALPHABETICALLY"
+            | "ALPHABETICALLY_REVERSE"
+            | "CAPITALISATION"
+            | "CAPITALISATION_REVERSE"
+            | "CHRONOLOGICALLY"
+            | "CHRONOLOGICALLY_REVERSE",
+          timeFormat: userSettings.timeFormat as
+            | "system"
+            | "12-hour"
+            | "24-hour",
+        };
+        const success = await datastore.save({ recordToSave });
+        if (success) {
+          logger.debug(
+            `Datastore initialised with current settings`,
+            userSettings
+          );
+        } else {
+          logger.error(`Datastore initialisation failed`, {
+            settings: userSettings,
+            errors: datastore.errors,
+          });
+          return;
+        }
+      } else {
+        const record = settingsRecords[0] as SettingsRecord;
+        // TODO find a better way to convert Settings state and Settings record
+        setUserSettings({
+          language: record.language,
+          theme: record.theme,
+          marketSelection: record.marketSelection,
+          marketSort: record.marketSort,
+          timeFormat: record.timeFormat,
+        });
+      }
+
+      datastoreInitialised.current = true;
+    };
+
+    const updateHandler = async () => {
+      logger.debug(`User Settings datastore update handler`);
+      // Updating the datastore with the update on current settings
+
+      const settingsRecords = await datastore.getMany();
+      const record = settingsRecords[0] as SettingsRecord;
+
+      // Get the _id and _rev from the record and the settings properties from the state
+      // TODO find a better way to cast the property types
+      const recordToSave: SettingsRecord = {
+        ...record,
+        language: userSettings.language as string,
+        theme: userSettings.theme as
+          | "system"
+          | "black"
+          | "dark"
+          | "light"
+          | "white",
+        marketSelection: userSettings.marketSelection as string[],
+        marketSort: userSettings.marketSort as
+          | "ALPHABETICALLY"
+          | "ALPHABETICALLY_REVERSE"
+          | "CAPITALISATION"
+          | "CAPITALISATION_REVERSE"
+          | "CHRONOLOGICALLY"
+          | "CHRONOLOGICALLY_REVERSE",
+        timeFormat: userSettings.timeFormat as "system" | "12-hour" | "24-hour",
+      };
+      const success = await datastore.save(recordToSave);
+      if (success) {
+        logger.debug(`User Settings saved in datastore`, userSettings);
+      } else {
+        logger.error(`Settings save in datastore failed`, {
+          settings: userSettings,
+          errors: datastore.errors,
+        });
+      }
+    };
+
+    if (datastoreInitialised.current === false) {
+      void initialisationHandler();
+    } else {
+      void updateHandler();
+    }
+  }, [userSettings, datastore]);
+
+  useEffect(() => {
+    if (localStorage && !datastore && datastoreInitialised.current === false) {
+      localStorage.setItem(
+        userSettingsLocalStorageKey,
+        JSON.stringify(userSettings)
+      );
+      logger.info(`User Settings saved in local storage`, userSettings);
+    }
+  }, [userSettings, datastore]);
 
   const contextValue = useMemo(() => {
     const setter = (key: string, value: string | string[]) => {
